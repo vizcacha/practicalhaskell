1a0c8207334d212b3b011bf9c26a09b451af0d1e
Steven Canning <spcanning@gmail.com>
2020-08-20 16:04:51 -0700

    Derive show in new data types

--- chapter2/src/Chapter2/DataTypes.hs
+++ chapter2/src/Chapter2/DataTypes.hs
@@ -9,7 +9,10 @@ data Person = Person String String Gender
             deriving Show
 
 data Gender = Male | Female | OtherGender
+            deriving Show
 
 data TimeMachine = TimeMachine String Int String TimeDirection Float
+            deriving Show
 
 data TimeDirection = Past | Future
+            deriving Show
