11e6e10664e84ca663ae2713ff27b48183656277
Steven Canning <spcanning@gmail.com>
2020-08-20 16:23:20 -0700

    Exercise 2.5

--- chapter2/src/Chapter2/DataTypes.hs
+++ chapter2/src/Chapter2/DataTypes.hs
@@ -16,3 +16,20 @@ data TimeMachine = TimeMachine String Int String TimeDirection Float
 
 data TimeDirection = Past | Future
             deriving Show
+
+countGenders :: [Client] -> (Int, Int)
+countGenders = countGenders' (0, 0)
+
+countGenders' :: (Int, Int) -> [Client] -> (Int, Int)
+countGenders' (male, female) ls = case ls of
+                                    []     -> (male, female)
+                                    (x:xs) -> case x of
+                                                Individual (Person _ _ g) _ -> case g of
+                                                                                 Male   -> countGenders' (male + 1, female) xs
+                                                                                 Female -> countGenders' (male, female + 1) xs
+                                                _                           -> countGenders' (male, female) xs
+
+discountMachines :: [TimeMachine] -> Float -> [TimeMachine]
+discountMachines ls discount = case ls of
+                                 [] -> []
+                                 ((TimeMachine manu model name dir price):xs) -> (TimeMachine manu model name dir ((1 - discount) * price)):(discountMachines xs discount)
